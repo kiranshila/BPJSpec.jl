8e85c78b9347bac9ae7d60f84f99944bb6c3168e
Kiran Shila <me@kiranshila.com>
2023-01-31 15:50:34 -0800

    back to original abstract block matrix

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,36 +9,36 @@ using Statistics
 Random.seed!(123)
 
 @testset "BPJSpec Tests" begin
-    # @testset "wrappers" begin
-    #     include("wrappers/FastTransformsWrapper.jl")
-    #     include("wrappers/CosmologyWrapper.jl")
-    #     include("wrappers/GSLWrapper.jl")
-    # end
-
-    # @testset "utilities" begin
-    #     include("utilities/misc.jl")
-    #     include("utilities/parallel.jl")
-    #     include("utilities/recombination-lines.jl")
-    #     include("utilities/white-noise.jl")
-    # end
-
-    # @testset "sky" begin
-    #     include("sky-components/foregrounds.jl")
-    #     include("sky-components/signal.jl")
-    # end
-
-    # @testset "interferometer" begin
-    #     include("interferometer/metadata.jl")
-    #     include("interferometer/baseline-hierarchy.jl")
-    #     include("interferometer/noise-model.jl")
-    # end
-
-    @testset "block-matrices" begin
-        include("block-matrices/storage-mechanisms.jl")
-        include("block-matrices/abstract-block-matrix.jl")
-        include("block-matrices/broadcasting.jl")
-        include("block-matrices/concrete-block-matrices.jl")
+    @testset "wrappers" begin
+        include("wrappers/FastTransformsWrapper.jl")
+        include("wrappers/CosmologyWrapper.jl")
+        include("wrappers/GSLWrapper.jl")
     end
+
+    @testset "utilities" begin
+        include("utilities/misc.jl")
+        include("utilities/parallel.jl")
+        include("utilities/recombination-lines.jl")
+        include("utilities/white-noise.jl")
+    end
+
+    @testset "sky" begin
+        include("sky-components/foregrounds.jl")
+        include("sky-components/signal.jl")
+    end
+
+    @testset "interferometer" begin
+        include("interferometer/metadata.jl")
+        include("interferometer/baseline-hierarchy.jl")
+        include("interferometer/noise-model.jl")
+    end
+
+    # @testset "block-matrices" begin
+    #     include("block-matrices/storage-mechanisms.jl")
+    #     include("block-matrices/abstract-block-matrix.jl")
+    #     include("block-matrices/broadcasting.jl")
+    #     include("block-matrices/concrete-block-matrices.jl")
+    # end
 
     # @testset "fundamentals" begin
     #     include("fundamentals/transfer-matrix.jl")
